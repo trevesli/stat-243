1ab70abf54dc75c967f0cb55a0b0c81e1aaf0abb
treves <treves@berkeley.edu>
2024-12-06 11:46:15 -0800

    Consolidated my changes

--- ars/ars.py
+++ ars/ars.py
@@ -160,12 +160,16 @@ def h_log(f, x):
 
 h_log.cache = {}
 
-def is_log_concave(f, x_range):
+def is_log_concave(f, x_range, eps=1e-10):
     """Checks if a function is log-concave over the given range."""
     x = np.asarray(x_range)
+    # Ensure no duplicates in range (for numerical differentiation)
+    if np.any(np.diff(x) <= 0):
+        raise ValueError("x_range need to be increasing.")
     f_values = f(x)
     if np.any(f_values <= 0):
         raise ValueError("Function values must be positive.")
+    f_values = np.maximum(f_values, eps)
     f_prime = np.gradient(f_values, x)
     log_deriv = f_prime / f_values
     is_descending = np.all(np.diff(log_deriv) <= 0)
